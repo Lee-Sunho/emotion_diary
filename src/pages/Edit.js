8d13362b904c9e9c3c54b52d7ea872f5d587dec0
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-01-24 21:16:24 +0900

    feat: 기본 페이지 구성

---
+++ src/pages/Edit.js
@@ -0,0 +1,26 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        홈으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
