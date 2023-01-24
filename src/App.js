c420150aebed0dfc6df7895483d4829011cdc3b3
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-01-24 21:20:25 +0900

    feat: 공통 컴포넌트 - MyButton, MyHeader 기초 작성 및 디자인

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
-    <div className="App"></div>
+    <BrowserRouter>
+      <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onclick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onclick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/new" element={<New />}></Route>
+          <Route path="/edit" element={<Edit />}></Route>
+          <Route path="/diary/:id" element={<Diary />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
