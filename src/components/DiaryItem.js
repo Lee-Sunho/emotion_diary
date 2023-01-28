77f2d8a180ec3d88ce97e6c101e430a83e1aa5a3
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-01-29 01:25:10 +0900

    feat: DiaryItem 클릭 이벤트 구현

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,8 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
+
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
+
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   const strDate = new Date(parseInt(date)).toLocaleDateString();
   return (
     <div className="DiaryItem">
       <div
+        onClick={goDetail}
         className={[
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
@@ -12,11 +26,13 @@ const DiaryItem = ({ id, emotion, content, date }) => {
           src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}
         ></img>
       </div>
-      <div className="info_wrapper">
+      <div onClick={goDetail} className="info_wrapper">
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
-      <div></div>
+      <div onClick={goEdit} className="btn_wrapper">
+        <MyButton text={"수정하기"} />
+      </div>
     </div>
   );
 };
