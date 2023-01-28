46c8505b18158a8c9529146d00342214ec8ce7e9
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-01-29 01:11:56 +0900

    feat: DiaryItem 이미지 부분 구현

---
+++ src/components/DiaryItem.js
@@ -0,0 +1,20 @@
+const DiaryItem = ({ id, emotion, content, date }) => {
+  return (
+    <div className="DiaryItem">
+      <div
+        className={[
+          "emotion_img_wrapper",
+          `emotion_img_wrapper_${emotion}`,
+        ].join(" ")}
+      >
+        <img
+          src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}
+        ></img>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default DiaryItem;
