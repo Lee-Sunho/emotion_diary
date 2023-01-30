96c6e539c4b8169362aae12f08c30e34d484c41e
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-01-30 19:47:24 +0900

    feat: 일기 수정 페이지 구현

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,24 +1,29 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
+  const [originData, setOriginData] = useState();
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { id } = useParams();
+
+  const diaryList = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      if (!targetDiary) {
+        navigate("/", { replace: true });
+      }
+      setOriginData(targetDiary);
+    }
+  }, [id, diaryList]);
   return (
     <div>
-      <button
-        onClick={() => {
-          navigate("/home");
-        }}
-      >
-        홈으로 가기
-      </button>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        뒤로 가기
-      </button>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
