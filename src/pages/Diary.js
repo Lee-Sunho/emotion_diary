306bb2abf44d1ad4c33bcc40d158871da3405e3d
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-01-30 22:10:39 +0900

    feat: Diary 상세 페이지 헤더 섹션 구현

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,11 +1,47 @@
-import { useParams } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+import { getStringDate } from "../util/date";
 
 const Diary = () => {
   const { id } = useParams();
-  console.log(id);
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      if (!targetDiary) {
+        alert("없는 일기입니다.");
+        navigate("/", { replace: true });
+      }
+      setData(targetDiary);
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  }
+
   return (
-    <div>
-      <h1>Diary</h1>
+    <div className="DiaryPage">
+      <MyHeader
+        headText={`${getStringDate(new Date(data.date))} 기록`}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          <MyButton
+            text={"수정하기"}
+            onClick={() => navigate(`/edit/${data.id}`)}
+          />
+        }
+      />
     </div>
   );
 };
